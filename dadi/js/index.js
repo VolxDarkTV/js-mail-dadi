8136bb5aea3ed4ce61d8e06527011992c9e19795
VolxDarkTV <vinvenzo.foti@outlook.it>
2022-11-10 15:39:50 +0100

    stabilito chi vince

--- dadi/js/index.js
+++ dadi/js/index.js
@@ -4,13 +4,20 @@ let startButton = document.getElementById("start-button");
 startButton.addEventListener("click", 
     function(){
         // Numero Utente
-        let userNumber = document.getElementById("dice-user").innerHTML = `numero: ` + (Math.floor(Math.random() * 6) + 1);
+        const userCasualNumber = Math.floor(Math.random() * 6) + 1;
+        const PcCasualNumber = Math.floor(Math.random() * 6) + 1;
+        let userNumber = document.getElementById("dice-user").innerHTML = `numero: ` + userCasualNumber;
 
         // Numero PC
-        let computerNumber = document.getElementById("dice-computer").innerHTML = `numero: ` + (Math.floor(Math.random() * 6) + 1);
+        let computerNumber = document.getElementById("dice-computer").innerHTML = `numero: ` + PcCasualNumber;
 
         // condizioni affinchè vinca un numero piùttosto che un'altro
         
+        if(userCasualNumber > PcCasualNumber){
+            let resultsView = document.getElementById("results-view").innerHTML = `Congratulazioni hai vinto!`
+        }else{
+            let resultsView = document.getElementById("results-view").innerHTML = `Ops! Hai perso!`
+        }
     }
 )
 
